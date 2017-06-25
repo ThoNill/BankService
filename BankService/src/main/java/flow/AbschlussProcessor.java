4ac1a0ccbd6a8c3e276fef30ef72c4a30c06e0b8
Thomas Nill <t.nill@t-online.de>
2017-06-25 07:32:55 +0200

    Umbenennen von FileProcessor AbschlussProcessor

--- BankService/src/main/java/flow/AbschlussProcessor.java
+++ BankService/src/main/java/flow/AbschlussProcessor.java
@@ -6,11 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.messaging.Message;
 
-public class FileProcessor {
-    Logger LOG = LogManager.getLogger(FileProcessor.class);
+public class AbschlussProcessor {
+    Logger LOG = LogManager.getLogger(AbschlussProcessor.class);
     
     public void process(Message<String> msg) {
         Object content = msg.getPayload();
-        LOG.debug("Object of Class [" + content.getClass().getCanonicalName() + "] is: " + content);
+        LOG.debug("Abschluss des Prozesses [" + content+ "]");
     }
 }
